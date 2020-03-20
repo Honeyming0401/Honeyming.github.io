89ed4ac2952e7bac0db4b9828ec8ef87618c0b6d
yeonggyulim <lyg940128@gmail.com>
2020-03-20 13:22:18 +0900

    add google analytics tackingId

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,7 +65,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: 'UA-XXXX-Y',
+        trackingId: 'UA-161412397-1',
         // Puts tracking script in the head instead of the body
         head: true,
         // IP anonymization for GDPR compliance
